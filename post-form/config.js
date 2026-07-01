d2f82f7d73102fe6d2b98a23973797e6a3c893f5
jmurabe <jmurabe@users.noreply.github.com>
2026-07-01 22:04:49 +0000

    deploy: fcea77ed4f76e1886aeb61567dc27d0636748bf3

---
+++ post-form/config.js
@@ -0,0 +1,9 @@
+window.LIFELOG_CONFIG = {
+  GOOGLE_CLIENT_ID: "74582613623-tqplo82cjouc5qj37laup1mlonp8kfnr.apps.googleusercontent.com",
+  // API_ENDPOINT: "http://localhost:8787/api/post", // 開発用
+  API_ENDPOINT:  "https://lifelog-api.jmurabe.workers.dev/api/post", //本番用
+
+  MAX_IMAGES: 5,
+  MAX_WIDTH: 800, // 縮小幅（px）
+  JPEG_QUALITY: 0.8
+};
